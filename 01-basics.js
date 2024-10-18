72014542a732a953d45faf98d729709f34882912
fatima Muhammad Yousuf <its_fatimamuhammadyousuf@gmail.com>
2024-10-18 13:12:04 +0500

    done

---
+++ 01-basics.js
@@ -0,0 +1,18 @@
+const accountId =  212053917
+let accountEmail = "fsy@gmail.com"
+var accountPassword = "qw123"
+accountAdress = "sukkur"
+let accountStatus;
+
+//accountId = 2; coz its const it never be changed
+
+console.log([accountId,accountEmail,accountPassword,accountAdress, accountStatus])
+// account id 2 not allowed
+
+accountEmail = "fast_fsy@gmail.com"
+accountPassword  = "232111"
+accountAdress = "khi"
+//console.log(accountId);
+
+//console.log([accountId,accountEmail,accountPassword,accountAdress,accountStatus, accountStatus])
+/* not to use var b/c of issue in block scope or functional scope */
