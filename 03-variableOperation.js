7c174e8d6f5ea93b2ce91acd78be9a4f70e5dc9c
fatima Muhammad Yousuf <its_fatimamuhammadyousuf@gmail.com>
2024-10-21 13:33:37 +0500

    conversion of datatypes

---
+++ 03-variableOperation.js
@@ -0,0 +1,56 @@
+/*let score = 33
+let myScore = null
+let scores = "33abc"
+let fsyScore= undefined
+let totalscore = false
+let mystring = "fsyshah"
+
+
+//dont know  k kia aya h score mai number ya string
+// const {score} = req.body
+//console.log(scores);
+
+console.log(typeof scores);
+//method
+console.log( typeof(score));
+
+// guarnatee to convert score into number
+//define type
+let valueInNumber = Number(myScore)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let valueInFsyScore = Number (fsyScore)
+console.log(typeof valueInFsyScore);
+console.log(valueInFsyScore);
+
+let valInTotalScore = Number (totalscore)
+console.log(typeof valInTotalScore);
+console.log(valInTotalScore);
+
+let valInFsyString = Number (mystring)
+console.log(typeof valInFsyString);
+console.log(valInFsyString); */
+
+// "33" => 33
+// "33abc" => NAN
+//"undefined" =>
+ // "true" => 1 , false => 0   
+
+// NAN is  A special number  that is mixed with string and number 
+// null is 0 type
+
+// NAN not a number 
+
+// let isLoggdIn = "FSY"
+// let booleanIsloogedIn = Boolean(isLoggdIn)
+// console.log(booleanIsloogedIn);
+
+// "" => FALSE
+// "FSY" => TRUE
+// 1 => TRUE, 0=> False
+
+let someNumber = 55
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
