61f6576c8f376b2b9c26723431f328efaf8a1fa8
fatima Muhammad Yousuf <its_fatimamuhammadyousuf@gmail.com>
2024-11-01 18:21:18 +0500

    done

---
+++ summaryDataTypes.js
@@ -0,0 +1,63 @@
+//primitive
+// non proimitive 
+// how u can store the values and access it based on it they categories into two.
+
+//Primitive 
+// seven categories 
+// call by value
+// jb bhi copy kraingy .inka refernece privide ni hoga but copy krk miljaye gi values , jo chnaging hogi wo copy mai hngi
+
+// primitiv
+// Number ,String , null empty, Bolean, undefined , Symbol,  BigInt
+//symbol used  to difeeerentiate unique bnany k lye 
+
+
+// Non Primitive
+//Array, OBJECTs , functions
+
+// browser Event achy sy smjh k krlain 
+
+// dyna,micallly type language and statically type 
+
+const score = 100
+const scoreValue = 100.3
+const IsLoggin = false
+const outsideTemp = null
+
+let useREmail;
+
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 898988989890900n
+console.log(bigNumber);
+
+//arrays
+const heros = ["fsy", "yy"]
+
+// object in variables 
+let myObj = {
+    name: "fsy",
+    age: 22,
+}
+// object in without variables
+{
+    name: "fsy"
+    ages: 2
+  //  present: false
+    // fruits: ["apple"]
+}
+const myfunction = function(){
+console.log("my world is my parents");
+console.log(myObj.age);
+
+
+}
+
+console.log(typeof bigNumber);
+// console.log(fruits);
+
